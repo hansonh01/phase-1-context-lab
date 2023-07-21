46749390129c0445e65b9596d8d3fa008ab8203d
Hanson <hanson.hua903@gmail.com>
2023-07-21 03:46:26 -0500

    Complete

--- index.js
+++ index.js
@@ -1,5 +1,66 @@
 /* Your Code Here */
 
+function createEmployeeRecord(array){
+    return {
+        firstName : array[0],
+        familyName : array[1],
+        title : array[2],
+        payPerHour : array[3],
+        timeInEvents : [],
+        timeOutEvents : []
+    };
+};
+
+function createEmployeeRecords(arrays){
+    const employeeRecords = [];
+
+    arrays.forEach((array)=>{
+        const employeeRecord = createEmployeeRecord(array);
+        employeeRecords.push(employeeRecord);
+    })
+
+    return employeeRecords;
+};
+
+function createTimeInEvent(dateStamp){
+    const [date, hour] = dateStamp.split(" ");
+    this.timeInEvents.push({
+        type : 'TimeIn',
+        hour : parseInt(hour, 10),
+        date : date,
+    });
+    return this;
+};
+
+function createTimeOutEvent(dateStamp){
+    const [date, hour] = dateStamp.split(" ");
+    this.timeOutEvents.push({
+        type : 'TimeOut',
+        hour : parseInt(hour, 10),
+        date : date,
+    });
+    return this;
+};
+
+function hoursWorkedOnDate (dateStamp){
+    const timeIn = this.timeInEvents.find((e)=> e.date === dateStamp);
+    const timeOut = this.timeOutEvents.find((e)=> e.date === dateStamp);
+
+    const tI = parseInt(timeIn.hour, 10);
+    const tO = parseInt(timeOut.hour, 10);
+
+    const totalHours = (tO - tI) / 100;
+
+    return totalHours;
+};
+
+function wagesEarnedOnDate(dateStamp){
+    const totalHours = hoursWorkedOnDate.call(this,dateStamp);
+    const payPerHour = this.payPerHour;
+
+    return totalHours * payPerHour;
+};
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -7,7 +68,8 @@
 
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
- */
+ 
+*/
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
@@ -19,5 +81,19 @@ const allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
-}
+};
 
+function findEmployeeByFirstName(collection, firstNameString){
+    return collection.find((employeeRecord)=>{
+        return employeeRecord.firstName === firstNameString;
+    });
+};
+
+function calculatePayroll(employeeRecords){
+    let totalPay = 0;
+    employeeRecords.forEach((employeeRecord)=>{
+        totalPay += allWagesFor.call(employeeRecord);
+    });
+
+    return totalPay;
+};
